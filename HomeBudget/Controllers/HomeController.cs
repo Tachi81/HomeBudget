b78d548baaddd379b178fff580f48ffbc789ad2c
Daniel Rużewicz <d.ruzew@gmail.com>
2018-04-05 02:30:14 +0200

    Changed Home controller a bit to display BankAccount index view

--- HomeBudget/Controllers/HomeController.cs
+++ HomeBudget/Controllers/HomeController.cs
@@ -3,14 +3,19 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using HomeBudget.Models;
 
 namespace HomeBudget.Controllers
 {
     public class HomeController : Controller
     {
+
+        private ApplicationDbContext db = new ApplicationDbContext();
+
+
         public ActionResult Index()
         {
-            return View();
+            return View(db.BankAccounts.ToList());
         }
 
         public ActionResult About()
