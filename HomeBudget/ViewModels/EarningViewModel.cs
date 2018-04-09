0188c18ff3a72aa7fd10fb9d1346dbb7f70cb53f
Daniel Rużewicz <d.ruzew@gmail.com>
2018-04-09 17:57:07 +0200

    Created Earning ViewModel

---
+++ HomeBudget/ViewModels/EarningViewModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using HomeBudget.Models;
+
+namespace HomeBudget.ViewModels
+{
+    public class EarningViewModel
+    {
+        public Earning Earning { get; set; }
+        public List<Earning> ListOfEarnings{ get; set; }
+
+        public IEnumerable<SelectListItem> SelectListOfBankAccounts { get; set; }
+        public IEnumerable<SelectListItem> SelectListOfExpenseCategories { get; set; }
+        public IEnumerable<SelectListItem> SelectListOfExpenseSubCategories { get; set; }
+    }
+}
