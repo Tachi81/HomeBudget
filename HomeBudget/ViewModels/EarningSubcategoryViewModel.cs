f2be4f0e4603074a5715c586338d0fd8a9ab64d0
Daniel Rużewicz <d.ruzew@gmail.com>
2018-04-11 00:48:15 +0200

    Alert. Alert! ViewBag detected ! Added ViewModels for Subcategories

---
+++ HomeBudget/ViewModels/EarningSubcategoryViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using HomeBudget.Models;
+
+namespace HomeBudget.ViewModels
+{
+    public class EarningSubcategoryViewModel
+    {
+        public EarningSubCategory SubCategory { get; set; }
+        public List<EarningSubCategory> ListOfEarningSubCategories { get; set; }
+
+        public IEnumerable<SelectListItem> SelectListOfEarningCategories { get; set; }
+    }
+}
