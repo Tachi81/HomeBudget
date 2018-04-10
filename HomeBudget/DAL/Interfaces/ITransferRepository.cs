8992fbec800895b52129f3c5d62db01f8151fac3
Daniel Rużewicz <d.ruzew@gmail.com>
2018-04-10 09:26:37 +0200

    Created Transfer repository and IRepository. Also created transfer viewModel

---
+++ HomeBudget/DAL/Interfaces/ITransferRepository.cs
@@ -0,0 +1,9 @@
+﻿using HomeBudget.DAL.Interfaces;
+using HomeBudget.Models;
+
+namespace HomeBudget.DAL.Repositories
+{
+    internal interface ITransferRepository : IAbstractRepository<Transfer>, IGetListWithIncludes<Transfer>
+    {
+    }
+}
---
+++ HomeBudget/ViewModels/TransferViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using HomeBudget.Models;
+
+namespace HomeBudget.ViewModels
+{
+    public class TransferViewModel
+    {
+        public Transfer Transfer { get; set; }
+        public List<Transfer> ListOfTransfers { get; set; }
+
+        public IEnumerable<SelectListItem> SelectListOfBankAccounts { get; set; }
+    }
+}
