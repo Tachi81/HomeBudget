217f87732578826ace528589296f0cbe4007ed3a
Daniel Rużewicz <d.ruzew@gmail.com>
2018-04-10 22:56:10 +0200

    Created parent classes: FinancialOperation, Category and SubCategory

---
+++ HomeBudget/Models/FinancialOperation.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using HomeBudget.DAL.Interfaces;
+
+namespace HomeBudget.Models
+{
+    public class FinancialOperation
+    {
+        public int Id { get; set; }
+        public double AmountOfMoney { get; set; }
+        public int BankAccountId { get; set; }
+        public int CategoryId { get; set; }
+        public int SubCategoryId { get; set; }
+
+        public int SourceBankAccountId { get; set; }
+        public int TargetBankAccountId { get; set; }
+
+        public DateTime DateTime { get; set; }
+        public string Note { get; set; }
+
+        public virtual BankAccount SourceBankAccount { get; set; }
+        public virtual BankAccount TargetBankAccount { get; set; }
+
+        public virtual BankAccount BankAccount { get; set; }
+        public virtual Category Category { get; set; }
+        public virtual SubCategory SubCategory { get; set; }
+    }
+}
---
+++ HomeBudget/Models/SubCategory.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace HomeBudget.Models
+{
+    public class SubCategory
+    {
+        public int Id { get; set; }
+        public string SubCategoryName { get; set; }
+        public int CategoryId { get; set; }
+
+        public virtual Category Category { get; set; }
+    }
+}
