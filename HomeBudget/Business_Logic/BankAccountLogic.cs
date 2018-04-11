8da41d3c301744077d10265efa2d23eacb4df8f1
Daniel Rużewicz <d.ruzew@gmail.com>
2018-04-12 00:51:40 +0200

    fixed copy/paste bug causing that account balance after transfer was counted in wrong direction

--- HomeBudget/Business_Logic/BankAccountLogic.cs
+++ HomeBudget/Business_Logic/BankAccountLogic.cs
@@ -29,9 +29,9 @@ namespace HomeBudget.Business_Logic
             {
                 var sumOfExpenses = _expensesRepository.GetWhere(t => t.BankAccountId == bankAccount.Id).Sum(e => e.AmountOfMoney);
                 var sumOfEarnings = _earningsRepository.GetWhere(t => t.BankAccountId == bankAccount.Id).Sum(e => e.AmountOfMoney);
-                var sumOfTransferIncomes = _transferRepository.GetWhere(t => t.SourceBankAccountId == bankAccount.Id)
+                var sumOfTransferIncomes = _transferRepository.GetWhere(t => t.TargetBankAccountId == bankAccount.Id)
                     .Sum(x => x.AmountOfMoney);
-                var sumOfTransferOutcomes = _transferRepository.GetWhere(t => t.TargetBankAccountId == bankAccount.Id)
+                var sumOfTransferOutcomes = _transferRepository.GetWhere(t => t.SourceBankAccountId == bankAccount.Id)
                     .Sum(t => t.AmountOfMoney);
                 bankAccount.Balance = bankAccount.InitialBalance - sumOfExpenses + sumOfEarnings + sumOfTransferIncomes - sumOfTransferOutcomes;
                 _bankAccountRepository.Update(bankAccount);
