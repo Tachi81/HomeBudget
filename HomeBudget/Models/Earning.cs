61826afec4a5e3b783aa23111b9e2c1e643741f9
Daniel Rużewicz <d.ruzew@gmail.com>
2018-04-12 00:16:28 +0200

    Fixed bug that history didn't show category and subcategory of expense/earning

--- HomeBudget/Models/Earning.cs
+++ HomeBudget/Models/Earning.cs
@@ -6,9 +6,7 @@ namespace HomeBudget.Models
 {
     public class Earning : FinancialOperation
     {
-        public new virtual EarningCategory Category { get; set; }
-        public new virtual EarningSubCategory SubCategory { get; set; }
-
+        
         public new int? SourceBankAccountId { get; set; }
         public new int? TargetBankAccountId { get; set; }
 
--- HomeBudget/Models/Expense.cs
+++ HomeBudget/Models/Expense.cs
@@ -5,9 +5,6 @@ namespace HomeBudget.Models
 {
     public class Expense : FinancialOperation
     {
-        public new virtual ExpenseCategory Category{ get; set; }
-        public new virtual ExpenseSubCategory SubCategory { get; set; }
-
         public new int? SourceBankAccountId { get; set; }
         public new int? TargetBankAccountId { get; set; }
 
