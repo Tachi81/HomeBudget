d146184d0372d8096b7fdc3077c849ea18fce107
Daniel Rużewicz <d.ruzew@gmail.com>
2018-04-11 18:33:59 +0200

    Created datepicker script. Added necessary things to bundles and to shared layout view. Then added class "datepicker" to other views

--- HomeBudget/App_Start/BundleConfig.cs
+++ HomeBudget/App_Start/BundleConfig.cs
@@ -10,6 +10,10 @@ namespace HomeBudget
             bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
                         "~/Scripts/jquery-{version}.js"));
 
+            bundles.Add(new ScriptBundle("~/bundles/jQuery-UI").Include(
+                "~/Scripts/jquery-ui-{version}.js"));
+
+
             bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
                         "~/Scripts/jquery.validate*"));
 
@@ -22,9 +26,17 @@ namespace HomeBudget
                       "~/Scripts/bootstrap.js",
                       "~/Scripts/respond.js"));
 
+            bundles.Add(new ScriptBundle("~/bundles/datepicker").Include(
+                "~/Scripts/DatePicker.js"
+                ));
+
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
                       "~/Content/site.css"));
+
+            bundles.Add(new StyleBundle("~/jQuery-UI/css").Include(
+                
+                "~/Content/themes/base/jquery-ui.min.css"));
         }
     }
 }
